ca150a0dd7d45a407245ee62beee89dd8acc3e9a
lunafish <lunafishjun@gmail.com>
2014-05-28 17:12:13 +0900

    change code for state machine

--- Assets/Script/LNPlayerCtrl.cs
+++ Assets/Script/LNPlayerCtrl.cs
@@ -2,18 +2,18 @@
 using System.Collections;
 
 public class LNPlayerCtrl : MonoBehaviour {
+	// public
+	public float _speed = 1.0f; // default player move speed
+	public float _rotate_speed = 1.0f; // default camera rotate speed
+	public GameObject _avatar; // avatar object
+	public GameObject _camera; // camera object
+	public bool _viewer = false;
 
-	public float _speed = 1.0f;
-	public float _rotate_speed = 1.0f;
-	public GameObject _avatar;
-	public GameObject _camera;
-
+	// for mouse
 	private Vector3 _mouse_start_pos;
 	private Vector3 _mouse_pos;
 
-	private Vector3 _avatar_lookat;
-	private Animator _anim;
-
+	// for touch
 	struct LNTouch {
 		public bool use;
 		public int id;
@@ -21,22 +21,45 @@ public class LNPlayerCtrl : MonoBehaviour {
 		public Vector3 pos;
 	};
 
-	private LNTouch[] _touches = new LNTouch[5];
+	// const value
+	private const int MAX_INPUT = 5;
+	private const int TOUCH_DOWN = 0;
+	private const int TOUCH_HOLD = 1;
+	private const int TOUCH_UP = 2;
 
+	private LNTouch[] _touches = new LNTouch[MAX_INPUT];
+
+	// for camera
 	private float _cam_length;
 	private Vector3 _cam_target_pos;
+	private Vector3 _avatar_lookat;
+
+	// playter animation
+	private Animator _anim;
+
+	// input
+	struct LNInput {
+		public bool use;
+		public int state;
+		public Vector3 org_pos;
+		public Vector3 pos;
+	};
+	private LNInput[] _inputs = new LNInput[MAX_INPUT];
+
 
 	// Use this for initialization
 	void Start () {
+		// set avatar look
 		_avatar_lookat = transform.position + transform.forward;
-		_anim = _avatar.GetComponent<Animator>();
+		_anim = _avatar.GetComponent<Animator>(); // get avatar animation controler
 
-		_touches[0].use = false;
-		_touches[1].use = false;
-		_touches[2].use = false;
-		_touches[3].use = false;
-		_touches[4].use = false;
+		// init value
+		for(int i=0; i < MAX_INPUT; i++) {
+			_touches[i].use = false;
+			_inputs[i].use = false;
+		}
 
+		// set camera at avatar pos
 		Vector3 v = transform.position - _camera.transform.position;
 		_cam_length = v.magnitude;
 		_cam_target_pos = _camera.transform.position;
@@ -47,8 +70,8 @@ public class LNPlayerCtrl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Translate ();	
-
+		UpdateInput();	
+		UpdatePlayer();
 	}
 
 	void Move(Vector3 v) {
@@ -62,7 +85,8 @@ public class LNPlayerCtrl : MonoBehaviour {
 			transform.Translate(mov); // move
 			_avatar_lookat = transform.position + vec;
 			_avatar.transform.LookAt( _avatar_lookat );
-			Update_camera ();
+			if(!_viewer)
+				Update_camera ();
 		}
 	}
 	
@@ -70,20 +94,26 @@ public class LNPlayerCtrl : MonoBehaviour {
 		if( Mathf.Abs( v.x ) > 10.0f ) {
 			float f = _rotate_speed * (v.x * 0.1f);
 			_camera.transform.Rotate(0.0f, f, 0.0f);
-			Update_camera ();
+			if(!_viewer)
+				Update_camera ();
 		}
 	}
 
 	bool isMoveTouch( int n ) {
-		float f = _touches[n].org_pos.x / Screen.width;
+		float f = _inputs[n].org_pos.x / Screen.width;
 		if (f < 0.5f) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	// Player translate
-	void Translate( ) {
+	void UpdateInput( ) {
+		// init input value
+		for(int i=0; i < MAX_INPUT; i++) {
+			_inputs[i].use = false;
+		}
+
 		#if UNITY_IPHONE
 		if (Input.touchCount > 0 ) {
 			for(int i = 0; i < Input.touchCount; i++) {
@@ -96,10 +126,16 @@ public class LNPlayerCtrl : MonoBehaviour {
 							_touches[j].id = Input.GetTouch(i).fingerId;
 							_touches[j].org_pos = Input.GetTouch(i).position;
 
+							_inputs[j].use = true;
+							_inputs[j].state = TOUCH_DOWN;
+							_inputs[j].org_pos = Input.GetTouch(i).position;
+
+							/*
 							// camera
 							if( isMoveTouch( j ) ) {
 								transform.rotation = _camera.transform.rotation;
 							}
+							*/
 
 							break;
 						}
@@ -112,6 +148,12 @@ public class LNPlayerCtrl : MonoBehaviour {
 					for(int j = 0; j < 5; j++) {
 						if(_touches[j].use == true && _touches[j].id == Input.GetTouch(i).fingerId ) {
 							_touches[j].pos = Input.GetTouch(i).position;
+
+							_inputs[j].use = true;
+							_inputs[j].state = TOUCH_HOLD;
+							_inputs[j].pos = Input.GetTouch(i).position;
+
+							/*
 							v = _touches[j].pos - _touches[j].org_pos;
 
 							if( isMoveTouch( j ) ) {
@@ -119,7 +161,8 @@ public class LNPlayerCtrl : MonoBehaviour {
 								Move ( v );
 							} else {
 								Rotate ( v );
-							}					
+							}
+							*/
 							break;
 						}
 					}
@@ -128,9 +171,15 @@ public class LNPlayerCtrl : MonoBehaviour {
 				else if(Input.GetTouch(i).phase == TouchPhase.Ended) {
 					for(int j = 0; j < 5; j++) {
 						if(_touches[j].use == true && _touches[j].id == Input.GetTouch(i).fingerId ) {
+
+							_inputs[j].use = true;
+							_inputs[j].state = TOUCH_UP;
+							_inputs[j].pos = Input.GetTouch(i).position;
+							/*
 							if( isMoveTouch( j ) ) {
 								_anim.SetBool ("isRun", false);
 							}
+							*/
 
 							_touches[j].use = false;
 						}
@@ -143,12 +192,23 @@ public class LNPlayerCtrl : MonoBehaviour {
 		if(Input.GetMouseButtonDown(0) == true) {
 			_mouse_start_pos = Input.mousePosition;
 
+			_inputs[0].use = true;
+			_inputs[0].state = TOUCH_DOWN;
+			_inputs[0].org_pos = Input.mousePosition;
+
+			/*
 			float f = _mouse_start_pos.x / Screen.width;
 			if(f < 0.5f) {
 				transform.rotation = _camera.transform.rotation;
 			}
+			*/
 		}
 		else if(Input.GetMouseButton(0) == true) {
+			_inputs[0].use = true;
+			_inputs[0].state = TOUCH_HOLD;
+			_inputs[0].pos = Input.mousePosition;
+
+			/*
 			_mouse_pos = Input.mousePosition;
 			Vector3 v = _mouse_pos - _mouse_start_pos;
 
@@ -159,11 +219,47 @@ public class LNPlayerCtrl : MonoBehaviour {
 			} else {
 				Rotate ( v );
 			}
+			*/
 		}
 		else if (Input.GetMouseButtonUp(0) == true) {
-			_anim.SetBool ("isRun", false);
+			_inputs[0].use = true;
+			_inputs[0].state = TOUCH_UP;
+			_inputs[0].pos = Input.mousePosition;
+
+			//_anim.SetBool ("isRun", false);
 		}
 		#endif
+	}
+
+	// state machine
+	void state_move( ) {
+		for(int i = 0; i < MAX_INPUT; i++) {
+			if(_inputs[i].use == true) {
+				if(_inputs[i].state == TOUCH_DOWN) {
+					if(isMoveTouch(i)) {
+						transform.rotation = _camera.transform.rotation;
+					}
+				}
+				else if(_inputs[i].state == TOUCH_HOLD) {
+					Vector3 v = _inputs[i].pos - _inputs[i].org_pos;
+					if( isMoveTouch( i ) ) {
+						_anim.SetBool ("isRun", true);
+						Move ( v );
+					} else {
+						Rotate ( v );
+					}					
+				}
+				else if(_inputs[i].state == TOUCH_UP) {
+					_anim.SetBool ("isRun", false);
+				}
+			}
+		}
+	}
+
+	// player update
+	void UpdatePlayer( ) {
+		// state machine
+		state_move( );
 	}
 
 	// camera update
